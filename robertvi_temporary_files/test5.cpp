4b45441b7d8eb7a4eaf079ccc356e99849138c3d
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2023-09-15 15:35:17 +0000

    added one more test

---
+++ robertvi_temporary_files/test5.cpp
@@ -0,0 +1,17 @@
+/*
+
+g++ -Wno-write-strings -Wno-deprecated -I./robertvi_temporary_files/boost_1_83_0 -lm -O2 robertvi_temporary_files/test5.cpp -o robertvi_temporary_files/test5
+
+*/
+
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/circle_layout.hpp>
+
+int main(int argc, char* argv[]){
+	
+  	return 1;
+}
+
+/*
+no errors
+*/
